1b8a375d001d01ca23616eb2038888525e5f3bca
Dmitry Valov <d_valov@mail.ru>
2017-07-26 15:33:38 +0300

    Implemented lessons 5-6

--- src/main/java/com/prolev/remindme/server/controller/ReminderController.java
+++ src/main/java/com/prolev/remindme/server/controller/ReminderController.java
@@ -1,22 +1,34 @@
 package com.prolev.remindme.server.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import com.prolev.remindme.server.entity.Remind;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 /**
  * Created by dmitryvalov on 23.07.17.
  */
-@Controller
+@RestController
 @RequestMapping("/reminder")
 public class ReminderController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String getReminder(ModelMap model) {
+    public Remind getReminder() {
 
-        return  "My Reminder!";
+        return  createMockRemind();
+    }
+
+    private Remind createMockRemind() {
+
+        Remind remind = new Remind();
+        remind.setId(1);
+        remind.setRemindDate(new Date());
+        remind.setTitle("My fisrt remind");
+
+        return remind;
     }
 }
